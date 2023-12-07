a225d5c48f2464f81d688765d1d263fc34d02a63
Ondrej Toman <126340449+ondradevcz@users.noreply.github.com>
2023-12-07 18:22:28 +0100

    Delete MiniAI directory

--- MiniAI/Data.py
+++
@@ -1,3 +0,0 @@
-l_greetings = ("Ahoj", "Čau", "Dobrý den", "Dobrý den", "Dobrý den",)
-l_ai_greetings = ("Ahoj", "Čau", "Dobrý den", "Dobrý den")
-l_end_convo = ("End", "end", "konec", "Konec", "KONEC", "končím", "Končím")
